70dc2f7620994bcd2fd65e9a6541dd9d595b0f1a
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2025-02-05 15:32:07 +0530

    Added signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [
+        User,
+        Report
+      ],
+      synchronize: true
+    }),
+    MessagesModule, UsersModule, ReportsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
