90e47bfb30f5af5cbac9e0ab87b41e201fe426cd
Ashutosh Pandey <ashutosh.pandey@dataunveil.com>
2025-02-05 15:48:37 +0530

    Exluding properties

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user-dto';
+import {
+    Body, ClassSerializerInterceptor, Controller, Delete, Get, Param,
+    Patch, Post, Query, UseInterceptors
+} from '@nestjs/common';
+
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user-dto';
+import { UpdateUserDto } from './dtos/update-user-dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +14,26 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
         return this.usersService.create(body.email, body.password);
+    }
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.usersService.findOne(+id);
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.usersService.remove(+id);
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        return this.usersService.update(+id, body);
+    }
+
+    @Get()
+    findUsers(@Query('email') email: string) {
+        return this.usersService.find(email);
     }
 }
